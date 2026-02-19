c81455a396e91b156f27e57944b295e2921d2458
kluczi <bartekcsgo04@gmail.com>
2026-02-19 23:39:20 +0100

    core: update engine

--- core/engine.py
+++ core/engine.py
@@ -6,18 +6,19 @@ from .context import Context
 
 
 class EngineResult:
-    def __init__(self, results: list[RuleResult], passed_rules: list[Rules], failed_rules: List[rules]):
+    def __init__(self, results: list[RuleResult], passed_rules: list[Rules], failed_rules: list[rules]):
         self.results=results
         self.passed_rules=passed_rules
         self.failed_rules=failed_rules
 
 
+
 class Engine:
     """Engine for evaluating rules."""
     
-    def __init__(self):
-        self.rules: List[Rule] = []
+    def __init__(self, rules:list[Rule]):
+        self.rules = rules
 
-    def add_rule(self, rule: Rule):
+    def run(self, rule: Rule):
         """Add a rule to the engine."""
         self.rules.append(rule)
