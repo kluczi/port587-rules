18c7bf8f7b4ee6b78a920fbbcde0c35917cc09fc
kluczi <bartekcsgo04@gmail.com>
2026-02-22 00:26:55 +0100

    core: refine Action and ActionType
    - Add string values to ActionType enum
    - Rename Action.type to action_type
    - Implement Action.to_dict()

--- core/action.py
+++ core/action.py
@@ -4,17 +4,21 @@ from typing import Callable, Dict, Any
 from enum import Enum
 
 class ActionType(Enum):
-    PAUSE_CAMPAIGN,
-    REDUCE_VOLUME,
-    INCREASE_VOLUME,
-    HARD_STOP
+    PAUSE_CAMPAIGN="pause_campaign",
+    REDUCE_VOLUME="reduce_volume",
+    INCREASE_VOLUME="increase_volume",
+    HARD_STOP="hard_stop"
 
 
 class Action:    
-    def __init__(self, type: ActionType, payload: dict[str, Any], source_rule: str):
-        self.type=type
+    def __init__(self, action_type: ActionType, payload: dict[str, Any], source_rule: str):
+        self.action_type=action_type
         self.payload=payload
         self.source_rule=source_rule
-    def to_dict(self, ):
-        
+    def to_dict(self):
+        return {
+            "action_type": self.action_type.name,
+            "payload": self.payload,
+            "source_rule": self.source_rule
+        }
     
